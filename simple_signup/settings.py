87ccfd098bdcdaf3df389bb2076f6567f5e7128b
aleksanderjster <a.ermolaev.ee@gmail.com>
2023-08-04 09:26:35 +0200

    signin_signout functionality implemented

--- simple_signup/settings.py
+++ simple_signup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e&6d$6g8$uoy%btzzo#tfl#9+*je8!5tb^w%7)huryj@=zwtdg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # signin/signout
+    'sign',
+    'protect',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,10 @@ ROOT_URLCONF = 'simple_signup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        # required for allauth
+        # 'DIRS': [],
+        # required for signin/signout
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +72,9 @@ TEMPLATES = [
         },
     },
 ]
+
+LOGIN_URL = 'sign/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'simple_signup.wsgi.application'
 
